68de2e67e17405a30404c03cacf9992d9765638b
mokshith <83539464+mokshith1618@users.noreply.github.com>
2022-08-18 12:17:47 +0530

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,113 @@
+let box1=document.getElementById("1");
+let box2=document.getElementById("2");
+let box3=document.getElementById("3");
+let box4=document.getElementById("4");
+let box5=document.getElementById("5");
+let box6=document.getElementById("6");
+let box7=document.getElementById("7");
+let box8=document.getElementById("8");
+let box9=document.getElementById("9");
+let res=document.getElementById("res")
+let btn=document.getElementById("btn")
+btn.onclick=()=>{
+    box1.textContent=""
+    box2.textContent="" 
+    box3.textContent=""
+    box4.textContent=""
+    box5.textContent=""
+    box6.textContent=""
+    box7.textContent=""
+    box8.textContent=""
+    box9.textContent=""
+}
+let players={
+    0:"player1",
+    1:"player2"
+}
+let player1=document.getElementById("player1")
+let player2=document.getElementById("player2")
+player1.addEventListener("change",(event)=>{
+    players[0]=player1.value
+})
+player2.addEventListener("change",(event)=>{
+    players[1]=player2.value
+})
+let item=1
+let onCheck=()=>{
+    if((box1.textContent==box2.textContent && box2.textContent==box3.textContent && box3.textContent!="")
+        ||(box4.textContent==box5.textContent && box5.textContent==box6.textContent && box6.textContent!="")|| 
+        (box7.textContent==box8.textContent && box8.textContent==box9.textContent && box9.textContent!="")|| 
+        (box1.textContent==box4.textContent && box4.textContent==box7.textContent && box7.textContent!="")||
+        (box2.textContent==box5.textContent && box5.textContent==box8.textContent && box8.textContent!="")||
+        (box3.textContent==box6.textContent && box6.textContent==box9.textContent && box9.textContent!="")||
+        (box1.textContent==box5.textContent && box5.textContent==box9.textContent && box9.textContent!="")||
+        (box3.textContent==box5.textContent && box5.textContent==box7.textContent && box7.textContent!="")){
+            if(item==0){
+                res.textContent=players[0]+" Win!!"
+            }
+            else{
+                res.textContent=players[1]+" Win!!"
+            }
+    }
+    else if(box1.textContent!="" && box2.textContent!="" && box3.textContent!=""&& box4.textContent!=""
+    && box5.textContent!=""&& box6.textContent!=""&& box7.textContent!=""&& box8.textContent!=""
+    && box9.textContent!=""){
+        res.textContent="It's a Tie!!"
+    }
+}
+let click=(box)=>{
+    let head=document.createElement("h1")
+    if(box.textContent!=""){
+        res.textContent="The box is Already filled!!"
+        return
+    }
+    res.textContent=""
+    if(item==1){
+        box.textContent=""
+        head.textContent="X"
+        item=0
+    }
+    else{
+        box.textContent=""
+        head.textContent="O"
+        item=1
+    }
+    head.style.fontSize="50px";
+    box.appendChild(head)
+} 
+box1.onclick=()=>{
+    click(box1)
+    onCheck()
+}
+box2.onclick=()=>{
+    click(box2)
+    onCheck()
+}
+box3.onclick=()=>{
+    click(box3)
+    onCheck()
+}
+box4.onclick=()=>{
+    click(box4)
+    onCheck()
+}
+box5.onclick=()=>{
+    click(box5)
+    onCheck()
+}
+box6.onclick=()=>{
+    click(box6)
+    onCheck()
+}
+box7.onclick=()=>{
+    click(box7)
+    onCheck()
+}
+box8.onclick=()=>{
+    click(box8)
+    onCheck()
+}
+box9.onclick=()=>{
+    click(box9)
+    onCheck()
+}
